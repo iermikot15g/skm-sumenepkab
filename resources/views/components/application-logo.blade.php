345d31f1b179dc47f857955bdc202053a613e379
iermikot15g <iermiko.t15g@gmail.com>
2026-06-11 13:25:47 +0700

    Perbaikan ketiga, ubah logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,11 +1,11 @@
-@props(['width' => 'auto', 'height' => '50'])
+@props(['width' => 'auto', 'height' => '80'])
 
 <a href="/">
     <img 
-        src="{{ asset('images/logo-sumenep-192x192.png') }}" 
+        src="{{ asset('images/lambang-sumenep.png') }}" 
         alt="Logo Pemerintah Kabupaten Sumenep" 
         width="{{ $width }}"
         height="{{ $height }}"
-        class="block h-12 w-auto"
+        class="block h-24 md:h-40 w-auto mx-auto"
     />
 </a>
